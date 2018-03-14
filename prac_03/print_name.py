583c70dfbd0dcc37f6838ebe31cf4fab95b62552
Kyle Ormonde <kyle.ormonde@my.jcu.edu.au>
2018-03-14 11:39:39 +1000

    Upload previous programs for edit

--- prac_03/print_name.py
+++ prac_03/print_name.py
@@ -1,10 +1,22 @@
-name = input("Name: ")
-while not name:  # while name == ''
-    print("Name cannot be blank.")
-    name = str(input("Name: "))
-#
-for i in range(1, len(name), 2):
-    print(name[i])
+def main():
+    name = get_name()
+    steps = int(input("How many steps between letters: "))
+    print_name(name, steps)
 
 
-print(name[1::2])
+def get_name():
+    name = input("Name: ")
+    # while name.strip(' ') == ''
+    while not name:
+        print("Name cannot be blank.")
+        name = str(input("Name: "))
+    return name
+
+
+def print_name(name, steps):
+    for i in range(0, len(name), steps):
+        print(name[i])
+        # print(name[1::2])
+
+
+main()
