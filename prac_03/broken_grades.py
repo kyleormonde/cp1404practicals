f61564d361155b0a4b1400e376faac3f59e1e2e6
Kyle Ormonde <kyle.ormonde@my.jcu.edu.au>
2018-03-14 12:11:11 +1000

    Update old programs to incorporate functions

--- prac_03/broken_grades.py
+++ prac_03/broken_grades.py
@@ -3,14 +3,25 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
+
 # TODO: Fix this!
 
-score = float(input("Enter score: "))
-if 90 <= score <= 100:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-elif 0 <= score < 50:
-    print("Bad")
-else:
-    print("Invalid score")
+def main():
+    score = float(input("Enter score: "))
+    result = find_result(score)
+    print(result)
+
+
+def find_result(score):
+    if score < 0 or score > 100:
+        result = "Invalid Score"
+    elif score < 50:
+        result = "Bad"
+    elif score < 90:
+        result = "Passable"
+    else:
+        result = "Excellent"
+    return result
+
+
+main()
