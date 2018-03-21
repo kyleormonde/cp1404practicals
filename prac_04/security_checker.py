38390765fc54f0594736b49d1df90d3572bdabf2
Kyle Ormonde <kyle.ormonde@my.jcu.edu.au>
2018-03-21 14:44:14 +1000

    Upload first prac intermediate exercise.

---
+++ prac_04/security_checker.py
@@ -0,0 +1,10 @@
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45',
+'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole',
+'InterpreterInterface', 'StartServer', 'bob']
+
+users_name = input("Name: ")
+if users_name in usernames:
+    print("Access Granted")
+
+else:
+    print("Access Denied")
