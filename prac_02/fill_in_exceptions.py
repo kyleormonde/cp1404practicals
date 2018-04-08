359d6e694f151a748f7c66de8a2fbbac84d3d458
Kyle Ormonde <kyle.ormonde@my.jcu.edu.au>
2018-04-08 13:34:51 +1000

    Finish prac_05: lists.

--- prac_02/fill_in_exceptions.py
+++ prac_02/fill_in_exceptions.py
@@ -3,12 +3,57 @@ CP1404/CP5632 - Practical
 Fill in the TODOs to complete the task
 """
 
-finished = False
-result = 0
-while not finished:
-    try:
-        result = int(input("Enter an integer: "))
-        finished = True
-    except ValueError:
-        print("Please enter a valid integer.")
-print("Valid result is:", result)
+
+def main():
+    title = 'The Godfather'
+    year = 0
+    category = 'Drama'
+    year = check_movie_year()
+    print("{} ({} from {}) added to movie list".format(title, category, year))
+
+
+def check_movie_year():
+    finished = False
+    while not finished:
+            try:
+                year = int(input("Year: "))
+                if year <= 0:
+                    print("Number must be >= 0")
+                else:
+                    finished = True
+                    return year
+            except ValueError:
+                print("Please enter a valid integer.")
+            except TypeError:
+                print("Please enter a valid type.")
+
+
+
+
+
+
+main()
+
+
+
+
+
+
+
+
+
+
+    # finished = False
+    # new_movie = []
+    # while not finished:
+    #     try:
+    #         title = input("Title: ")
+    #         year = int(input("Year: "))
+    #         category = input("Category: ")
+    #         print("{} ({} from {}) added to movie list".format(title, category, year))
+    #         new_movie = [title, year, category, 'n']
+    #         finished = True
+    #     except ValueError:
+    #         print("Invalid value")
+    #     except TypeError:
+    #         print("Invalid type")
